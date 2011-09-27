6f1f650934193b0580705a7c349752cf2c55f12e
Tahir Butt <tahir.butt@gmail.com>
2011-09-26 23:36:54 -0400

    getting rid of old emacs configs

--- emacs.d/elpa/magit-0.8.1/magit-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; magit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (magit-status) "magit" "magit.el" (19787 37953))
-;;; Generated autoloads from magit.el
-
-(autoload 'magit-status "magit" "\
-Not documented
-
-\(fn DIR)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("magit-pkg.el") (19787 37953 592642))
-
-;;;***
-
-(provide 'magit-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; magit-autoloads.el ends here
