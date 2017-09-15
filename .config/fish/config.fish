c0949856fd5fba13bfe02b0c8378e0c934c877d4
Tahir H. Butt <tahir.butt@gmail.com>
2017-09-15 09:52:43 -0400

    Adding i3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,9 @@
 set PATH $PATH $HOME/.local/bin
-set PATH $PATH /opt/anaconda/bin
+set PATH $PATH $HOME/.gem/ruby/2.4.0/bin/
+
+set -gx TERM 'xterm-256color'
+set -gx CLICOLOR 1
+set -gx EDITOR nvim
+
+. $HOME/.config/fish/base16-material.dark.fish
+. /opt/anaconda/etc/fish/conf.d/conda.fish
