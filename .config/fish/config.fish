229e1d06a306bcff1f919842ef5086521cd1abf2
Tahir Butt <tahir.butt@gmail.com>
2018-01-04 14:56:15 -0500

    Nord for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,18 @@ set -gx TERM 'xterm-256color'
 set -gx CLICOLOR 1
 set -gx EDITOR nvim
 
-. $HOME/.config/fish/base16-material.dark.fish
+# bobthefish
+
+set -g theme_nerd_fonts yes
+
 . /opt/anaconda/etc/fish/conf.d/conda.fish
+
+# Base16 Shell
+if status --is-interactive
+    eval sh $HOME/.config/base16-shell/scripts/base16-nord.sh
+end
+
+# Set up fisherman
 if not test -f ~/.config/fish/functions/fisher.fish
   echo "Installing fisherman for the first time"
   curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
