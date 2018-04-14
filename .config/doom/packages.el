00163b92da812b232b562729c980328466d106ae
Tahir Butt <tahir.butt@gmail.com>
2018-04-14 14:03:55 -0400

    Removing doom into its own repo

--- .config/doom/packages.el
+++
@@ -1,9 +0,0 @@
-;;; packages.el --- description -*- lexical-binding: t; -*-
-
-(package! zotxt)
-(package! shr-tag-pre-highlight)
-(package! html2org)
-(package! evil-snipe :disable t)
-
-(provide 'packages)
-;;; packages.el ends here
