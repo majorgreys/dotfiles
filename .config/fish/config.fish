1bb062d4a3e136e3fcb7eca093cde44f670a785f
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-06-09 12:12:16 -0400

    Cleanup fish, city lights for Alacritty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,35 +1,32 @@
+set fish_greeting
+set -e fish_user_paths
+
 if test $HOME/.local/bin
-   set -U fish_user_paths $HOME/.local/bin/ $fish_user_paths
+   set -x fish_user_paths $HOME/.local/bin/ $fish_user_paths
 end
 
 if test $HOME/.asdf
-   set -U fish_user_paths $HOME/.asdf/bin/ $fish_user_paths
+   set -x fish_user_paths $HOME/.asdf/bin/ $fish_user_paths
    source ~/.asdf/asdf.fish
 end
 
 if type -q yarn
-    set -U fish_user_paths (yarn global bin) $fish_user_paths
+    set -x fish_user_paths (yarn global bin) $fish_user_paths
 end
 
 if type -q cargo
-    set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+    set -x fish_user_paths $HOME/.cargo/bin $fish_user_paths
 end
 
 if type -q go
-    set -U GOROOT (go env GOROOT)
-    set -U GOPATH (go env GOPATH)
-    set -U fish_user_paths $GOROOT/bin $fish_user_paths
-    set -U fish_user_paths $GOPATH/bin $fish_user_paths
+    set -x GOROOT (go env GOROOT)
+    set -x GOPATH (go env GOPATH)
+    set -x fish_user_paths $GOROOT/bin $fish_user_paths
+    set -x fish_user_paths $GOPATH/bin $fish_user_paths
 end
 
 if type -q exa
     abbr -a ls exa
-end
-
-# Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
 end
 
 # Set up fisherman
@@ -38,4 +35,3 @@ if not test -f ~/.config/fish/functions/fisher.fish
   curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
   fisher
 end
-
