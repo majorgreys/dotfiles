a995f1c5e68a58520f71747fb2a61f1bfe8fac5d
Tahir H. Butt <tahir.butt@datadoghq.com>
2025-04-21 22:34:00 -0400

    update doom

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -5,10 +5,12 @@
 (setq
  user-mail-address "tahir@tahirbutt.com"
  user-full-name    "Tahir H. Butt"
- doom-theme 'modus-operandi
- doom-font (font-spec :family "Essential PragmataPro" :size 14)
- doom-unicode-font (font-spec :family "Essential PragmataPro")
- doom-big-font (font-spec :family "Essential PragmataPro" :size 22))
+ doom-theme 'catppuccin
+ catppuccin-flavor 'macchiato
+ doom-font (font-spec :family "PragmataPro Mono Liga" :size 14)
+ doom-unicode-font (font-spec :family "PragmataPro Mono Liga")
+ doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 14)
+ doom-big-font (font-spec :family "PragmataPro Mono Liga" :size 22))
 
 (when IS-MAC
   (setq ns-use-thin-smoothing t)    ; thinner strokes for font-smoothing
@@ -17,43 +19,54 @@
   (add-to-list 'default-frame-alist '(ns-appearance . dark)))
 
 ;; lsp
-(after! lsp-mode
-  (setq lsp-log-io nil
-        lsp-file-watch-threshold 4000
-        lsp-headerline-breadcrumb-enable t
-        lsp-headerline-breadcrumb-segments '(file symbols)
-        lsp-imenu-index-symbol-kinds '(File Module Namespace Package Class Method Enum Interface
-                                       Function Variable Constant Struct Event Operator TypeParameter)
-        )
-  (dolist (dir '("[/\\\\]\\.ccls-cache\\'"
-                 "[/\\\\]\\.mypy_cache\\'"
-                 "[/\\\\]\\.pytest_cache\\'"
-                 "[/\\\\]\\.riot\\'"
-                 "[/\\\\]\\.ddriot\\'"
-                 "[/\\\\]\\.tox\\'"
-                 "[/\\\\]\\.ddtox\\'"
-                 "[/\\\\]\\.cache\\'"
-                 "[/\\\\]\\.clwb\\'"
-                 "[/\\\\]__pycache__\\'"
-                 "[/\\\\]build\\'"
-                 "[/\\\\]bazel-bin\\'"
-                 "[/\\\\]bazel-code\\'"
-                 "[/\\\\]bazel-genfiles\\'"
-                 "[/\\\\]bazel-out\\'"
-                 "[/\\\\]bazel-testlogs\\'"
-                 "[/\\\\]third_party\\'"
-                 "[/\\\\]third-party\\'"
-                 "[/\\\\]buildtools\\'"
-                 "[/\\\\]out\\'"))
-    (push dir lsp-file-watch-ignored-directories)))
+;; (after! lsp-mode
+;;   (setq lsp-log-io nil
+;;         lsp-file-watch-threshold 4000
+;;         lsp-headerline-breadcrumb-enable t
+;;         lsp-headerline-breadcrumb-segments '(file symbols)
+;;         lsp-imenu-index-symbol-kinds '(File Module Namespace Package Class Method Enum Interface
+;;                                        Function Variable Constant Struct Event Operator TypeParameter)
+;;         )
+;;   (dolist (dir '("[/\\\\]\\.ccls-cache\\'"
+;;                  "[/\\\\]\\.mypy_cache\\'"
+;;                  "[/\\\\]\\.pytest_cache\\'"
+;;                  "[/\\\\]\\.riot\\'"
+;;                  "[/\\\\]\\.ddriot\\'"
+;;                  "[/\\\\]\\.tox\\'"
+;;                  "[/\\\\]\\.ddtox\\'"
+;;                  "[/\\\\]\\.cache\\'"
+;;                  "[/\\\\]\\.clwb\\'"
+;;                  "[/\\\\]__pycache__\\'"
+;;                  "[/\\\\]build\\'"
+;;                  "[/\\\\]bazel-bin\\'"
+;;                  "[/\\\\]bazel-code\\'"
+;;                  "[/\\\\]bazel-genfiles\\'"
+;;                  "[/\\\\]bazel-out\\'"
+;;                  "[/\\\\]bazel-testlogs\\'"
+;;                  "[/\\\\]third_party\\'"
+;;                  "[/\\\\]third-party\\'"
+;;                  "[/\\\\]buildtools\\'"
+;;                  "[/\\\\]out\\'"
+;;                  "[/\\\\]bin\\'"
+;;                  "[/\\\\]ci\\'"
+;;                  "[/\\\\]ci-app\\'"
+;;                  "[/\\\\]etc\\'"
+;;                  "[/\\\\]ops\\'"
+;;                  "[/\\\\]spinnaker\\'"
+;;                  "[/\\\\]tilt\\'"
+;;                  "[/\\\\]vendor\\'"
+;;                  "[/\\\\]k8s\\'")))
+;;   (push dir lsp-file-watch-ignored-directories))
 
-(after! lsp-ui
-  (setq lsp-ui-doc-enable nil
-        lsp-lens-enable nil
-        lsp-ui-sideline-enable nil
-        lsp-ui-doc-include-signature t
-        lsp-ui-doc-max-height 15
-        lsp-ui-doc-max-width 100))
+;; (after! lsp-ui
+;;   (setq lsp-ui-doc-enable nil
+;;         lsp-lens-enable nil
+;;         lsp-ui-sideline-enable nil
+;;         lsp-ui-doc-include-signature t
+;;         lsp-ui-doc-max-height 15
+;;         lsp-ui-doc-max-width 100))
+
+(setq writeroom-fullscreen-effect t)
 
 ;; not sure why but projectile-generic-command is not getting set correctly
 ;; (when (executable-find "rg")
--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -36,21 +36,21 @@
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;tabs              ; a tab bar for Emacs
+       tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       (zen +writeroom-mode)              ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -119,7 +119,7 @@
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       (data +tree-sitter)              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
@@ -138,9 +138,9 @@
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
-       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (json +tree-sitter)              ; At least it ain't XML
+       (java +lsp +tree-sitter)       ; the poster child for carpal tunnel syndrome
+       (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -171,7 +171,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       (yaml +tree-sitter)              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
