b1526822d18b4b03a886583e94a3b8be4c11c3fd
Tahir Butt <tb@lotus.yt>
2025-10-11 07:07:33 -0400

    chore(fish): remove pyenv and work stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,6 @@ if test $HOME/.local/bin
     fish_add_path $HOME/.local/bin/
 end
 
-if type -q cargo
-    set -x fish_user_paths $HOME/.cargo/bin $fish_user_paths
-end
-
 if type -q go
     set -x GOROOT (go env GOROOT)
     set -x GOPATH (go env GOPATH)
@@ -40,10 +36,3 @@ if test $TERM = dumb
 end
 
 direnv hook fish | source
-
-pyenv init - | source
-
-# Created by `userpath` on 2024-09-17 19:06:55
-set PATH $PATH /home/bits/.local/share/hatch/pythons/3.12/python/bin
-abbr -a -- upgrade_ddtool 'curl -sL https://binaries.ddbuild.io/ddtool/v(curl -L https://binaries.ddbuild.io/ddtool/LATEST 2>/dev/null | cut -dv -f2)/ddtool.tar.gz | tar --warning=no-unknown-keyword -xOzf - ./ddtool_linux_amd64 > ~/.local/bin/ddtool && chmod +x ~/.local/bin/ddtool'
-export GOPATH='/home/bits/go'
