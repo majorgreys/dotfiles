7c528d7e2c061e538b2cc6fa882b4c71909cda5a
Tahir Butt <tb@lotus.yt>
2025-10-19 05:59:49 -0400

    chore: consolidate configs with stow and cleanup
    - Add doom and starship to stow management
    - Update doom configs to latest versions (Oct 11-18)
    - Remove fish catppuccin themes from git (let fisher manage)
    - Add catppuccin/fish to fish_plugins
    - Fix vim config by removing broken dein plugin manager
    - Update setup.sh to stow doom and starship configs

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,78 +1,79 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq shell-file-name (executable-find "bash"))
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
-(setq
- user-mail-address "tahir@tahirbutt.com"
- user-full-name    "Tahir H. Butt"
- doom-theme 'catppuccin
- catppuccin-flavor 'macchiato
- doom-font (font-spec :family "PragmataPro Mono Liga" :size 14)
- doom-unicode-font (font-spec :family "PragmataPro Mono Liga")
- doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 14)
- doom-big-font (font-spec :family "PragmataPro Mono Liga" :size 22))
 
-(when IS-MAC
-  (setq ns-use-thin-smoothing t)    ; thinner strokes for font-smoothing
-  ;; macOS natural title bars
-  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-  (add-to-list 'default-frame-alist '(ns-appearance . dark)))
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
-;; lsp
-;; (after! lsp-mode
-;;   (setq lsp-log-io nil
-;;         lsp-file-watch-threshold 4000
-;;         lsp-headerline-breadcrumb-enable t
-;;         lsp-headerline-breadcrumb-segments '(file symbols)
-;;         lsp-imenu-index-symbol-kinds '(File Module Namespace Package Class Method Enum Interface
-;;                                        Function Variable Constant Struct Event Operator TypeParameter)
-;;         )
-;;   (dolist (dir '("[/\\\\]\\.ccls-cache\\'"
-;;                  "[/\\\\]\\.mypy_cache\\'"
-;;                  "[/\\\\]\\.pytest_cache\\'"
-;;                  "[/\\\\]\\.riot\\'"
-;;                  "[/\\\\]\\.ddriot\\'"
-;;                  "[/\\\\]\\.tox\\'"
-;;                  "[/\\\\]\\.ddtox\\'"
-;;                  "[/\\\\]\\.cache\\'"
-;;                  "[/\\\\]\\.clwb\\'"
-;;                  "[/\\\\]__pycache__\\'"
-;;                  "[/\\\\]build\\'"
-;;                  "[/\\\\]bazel-bin\\'"
-;;                  "[/\\\\]bazel-code\\'"
-;;                  "[/\\\\]bazel-genfiles\\'"
-;;                  "[/\\\\]bazel-out\\'"
-;;                  "[/\\\\]bazel-testlogs\\'"
-;;                  "[/\\\\]third_party\\'"
-;;                  "[/\\\\]third-party\\'"
-;;                  "[/\\\\]buildtools\\'"
-;;                  "[/\\\\]out\\'"
-;;                  "[/\\\\]bin\\'"
-;;                  "[/\\\\]ci\\'"
-;;                  "[/\\\\]ci-app\\'"
-;;                  "[/\\\\]etc\\'"
-;;                  "[/\\\\]ops\\'"
-;;                  "[/\\\\]spinnaker\\'"
-;;                  "[/\\\\]tilt\\'"
-;;                  "[/\\\\]vendor\\'"
-;;                  "[/\\\\]k8s\\'")))
-;;   (push dir lsp-file-watch-ignored-directories))
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-;; (after! lsp-ui
-;;   (setq lsp-ui-doc-enable nil
-;;         lsp-lens-enable nil
-;;         lsp-ui-sideline-enable nil
-;;         lsp-ui-doc-include-signature t
-;;         lsp-ui-doc-max-height 15
-;;         lsp-ui-doc-max-width 100))
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
 
-(setq writeroom-fullscreen-effect t)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
 
-;; not sure why but projectile-generic-command is not getting set correctly
-;; (when (executable-find "rg")
-;;   (setq-default projectile-generic-command "rg --files --hidden -0"))
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
 
-;; https://discord.com/channels/406534637242810369/1178820376818618448/1179167822044205157
-;; (setq projectile-indexing-method 'alien)
+;; Set TERMINFO for Ghostty terminal support
+;; This allows emacsclient to work properly with Ghostty's xterm-ghostty terminal type
+(setenv "TERMINFO" "/Applications/Ghostty.app/Contents/Resources/terminfo")
 
-(provide 'config)
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
