fac1256931fdbc1162473d7239dfbe4df6f37f6b
Tahir Butt <tb@lotus.yt>
2025-12-13 19:03:54 -0500

    remove org-roam-skill package

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -2,14 +2,12 @@
 ;;; $DOOMDIR/packages.el
 
 (package! catppuccin-theme)
-(package! claude-code-ide :recipe (:type git :host github :repo "manzaltu/claude-code-ide.el"))
 (package! git-auto-commit-mode)
 
 ;; Personal org-mode packages
 (package! consult-org-roam)
 (package! org-super-agenda)
 (package! org-tidy)
-(package! org-roam-skill :recipe (:type git :host github :repo "majorgreys/org-roam-skill"))
 
 ;; Load local packages (not synced to repo)
 (load! "packages-local" nil t)
