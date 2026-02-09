e7cd9d509125a78dcc13a2630f9ef7c6c9dcf8b6
Tahir H. Butt <tahir.butt@datadoghq.com>
2026-02-09 11:33:32 -0500

    Remove +extra from ligatures module

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -36,7 +36,7 @@
        (emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
-       (ligatures +extra +pragmata-pro)         ; ligatures and symbols to make your code pretty again
+       (ligatures +pragmata-pro)         ; font ligatures only, no symbol substitutions
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
